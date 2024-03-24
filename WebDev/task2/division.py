c5ea06004b16ae388de4aa52d3760d71345555ad
Altynaim12 <123297782+Altynaim12@users.noreply.github.com>
2024-03-24 20:21:53 +0500

    Delete WebDev/task2 directory

--- WebDev/task2/division.py
+++
@@ -1,9 +0,0 @@
-# Reading input integers from standard input
-a = int(input().strip())
-b = int(input().strip())
-
-# Printing the result of integer division (a // b)
-print(a // b)
-
-# Printing the result of float division (a / b)
-print(a / b)
--- WebDev/task2/loops.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    n = int(input().strip())
-
-    # Loop through all non-negative integers less than n
-    for i in range(n):
-        # Print the square of each number
-        print(i * i)
